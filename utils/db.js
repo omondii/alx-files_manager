8e107149b3cc27b0d8db37196f264819f4eada0f
Brian Omondi <omondiibrian00@gmail.com>
2024-03-10 00:16:06 +0300

    Commented module

--- utils/db.js
+++ utils/db.js
@@ -10,27 +10,47 @@ class DBClient {
         const host = process.env.DB_HOST || 'localhost';
         const port = process.env.DB_PORT || 27017;
         const database = process.env.DB_DATABASE || 'files_manager';
-
         const url = `mongodb://${host}:${port}/${database}`;
 
-        this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
-        this.client.connect()
-            .then(() => console.log('Connected'))
-            .catch(error => console.error('error'));      
+        MongoClient.connect(url, { useUnifiedTopology: true })
+        .then((client) => {
+            this.db = client.db(database);
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
+            this.db = null;
+        });      
     }
 
+    /**
+     * Checks if connection was successful or failed
+     * @returns {boolean}
+     */
     isAlive() {
-        return this.client.isConnected();
+        if (this.db) {
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * Returns the number of documents in the collection users
+     * @returns {int}
+     */
     async nbUsers() {
-        const count = await this.client.db().collection('files').countDocuments();
-        return count;
-    } catch (error) {
-        console.error('Error');
-        throw error;
+        try {
+            const count = await this.client.db().collection('files').countDocuments();
+            return count;
+        } catch (error) {
+            console.error('Error');
+            throw error;
+        }
     }
 
+    /**
+     * Returns the number of documents in the collection files
+     * @returns {int}
+     */
     async nbFiles() {
         try {
             const count = await this.client.db().collection('files').countDocuments();
